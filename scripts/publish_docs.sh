2ce461e167f426fa4ad015a422dbee19d5c3b7c8
Mikhail Kuklin <mihan007@ya.ru>
2020-04-17 11:52:30 +0300

    Use installed node
    Re #6

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo start publishing docs
-stoplight publish --token "$1" --url https://stoplight.io/api
+node stoplight publish --token "$1" --url https://stoplight.io/api
 echo docs published
