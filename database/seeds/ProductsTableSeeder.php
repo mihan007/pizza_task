c3762c979bd7e44ca50df9292db9f03cac3fb54d
Mikhail Kuklin <mihan007@ya.ru>
2020-04-17 15:32:08 +0300

    Make different images for pizza
    Close #6

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -17,10 +17,13 @@ class ProductsTableSeeder extends Seeder
         foreach (Pizza::LIST as $title => $description) {
             $pizzaEurCost = random_int(500, 1500);
             $pizzaUsdCost = round($pizzaEurCost * $rate->getValue());
+            $imdWidth = random_int(415, 830);
+            $aspectRatio = 628 / 415;
+            $imdHeight = round($imdWidth * $aspectRatio);
             $pizza = [
                 'title' => $title,
                 'description' => $description,
-                'image' => 'https://img.pizza/415/628',
+                'image' => "https://img.pizza/{$imdWidth}/{$imdHeight}",
                 'price_eur' => $pizzaEurCost,
                 'price_usd' => $pizzaUsdCost,
                 'rating' => random_int(2, 5)
