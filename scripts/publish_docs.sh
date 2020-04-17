081db3484dfcd7ca2e38e8cc351d98aa41866dff
Mikhail Kuklin <mihan007@ya.ru>
2020-04-17 11:38:25 +0300

    Let's try install stoplight globally
    Re #6

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-npm install --silent
-echo npm packages installed
-./node_modules/@stoplight/cli/bin/stoplight.js publish --token "$1" --url https://stoplight.io/api
-echo published docs
+echo start publishing docs
+stoplight publish --token "$1" --url https://stoplight.io/api
+echo docs published
