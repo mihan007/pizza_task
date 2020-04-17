190a2e7c1446e3410adb4487b53bb541e52c9a12
Mikhail Kuklin <mihan007@ya.ru>
2020-04-17 12:06:41 +0300

    Make stoplight work
    Re #6

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 echo which stoplight
 which stoplight
+echo which node
+which node
 echo start publishing docs
-node /usr/local/lib/node_modules/stoplight publish --token "$1" --url https://stoplight.io/api
+node /home/travis/.nvm/versions/node/v13.13.0/bin/stoplight publish --token "$1" --url https://stoplight.io/api
 echo docs published
