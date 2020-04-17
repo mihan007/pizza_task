eb94a1f0a24f9a84f588e642935c87a4bbf4517d
Mikhail Kuklin <mihan007@ya.ru>
2020-04-17 11:56:33 +0300

    Correct path no stoplight
    Re #6

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo start publishing docs
-node stoplight publish --token "$1" --url https://stoplight.io/api
+node /usr/local/bin/stoplight publish --token "$1" --url https://stoplight.io/api
 echo docs published
