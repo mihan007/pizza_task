8c69ad741f8d03a0a2eac3174a6c5ef49869670f
Mikhail Kuklin <mihan007@ya.ru>
2020-04-17 11:32:49 +0300

    Exclude npm install from deploy
    Re #6

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+npm install --silent
+echo npm packages installed
 ./node_modules/@stoplight/cli/bin/stoplight.js publish --token "$1" --url https://stoplight.io/api
+echo published docs
