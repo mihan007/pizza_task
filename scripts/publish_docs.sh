ae3e611536f9c1ca35c4b89ed1af87ba6fdbf1f0
Mikhail Kuklin <mihan007@ya.ru>
2020-04-17 12:02:42 +0300

    Check witch stoplight
    Re #6

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo which stoplight
+which stoplight
 echo start publishing docs
 node /usr/local/lib/node_modules/stoplight publish --token "$1" --url https://stoplight.io/api
 echo docs published
