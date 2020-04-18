2a8aa187226a5498b86e0710eb04380df855729a
Mikhail Kuklin <mihan007@ya.ru>
2020-04-18 20:00:23 +0300

    Truncate products before seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
+    protected $toTruncate = ['products'];
+
     /**
      * Seed the application's database.
      *
@@ -11,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
+        foreach($this->toTruncate as $table) {
+            DB::table($table)->truncate();
+        }
         $this->call(ProductsTableSeeder::class);
+        Model::reguard();
     }
 }
